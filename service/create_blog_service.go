7c87784f742276ce26e4e43edf935b36393c264c
yizhixiaokong <wdhxx20121@126.com>
2020-03-30 16:04:21 +0800

    前后端调用！

--- service/create_blog_service.go
+++ service/create_blog_service.go
@@ -7,8 +7,8 @@ import (
 
 // CreateBlogService 创建博客服务
 type CreateBlogService struct {
-	Title   string `form:"title" json:"title" binding:"required,min=2,max=30"`       //标题，最少2字最大30字
-	Details string `form:"details" json:"details" binding:"required,min=0,max=1000"` //内容，最少0字最大1000字
+	Title   string `form:"title" json:"title" binding:"required,min=1,max=30"`       //标题，最少1字最大30字
+	Details string `form:"details" json:"details" binding:"required,min=0,max=2000"` //内容，最少0字最大2000字
 }
 
 // Create 创建博客
