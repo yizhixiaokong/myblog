9557b6f8a409a6d187425ead98c50dd2878007e0
yizhixiaokong <wdhxx20121@126.com>
2021-07-12 21:12:16 +0800

    update:最新版本

--- service/delete_blog_service.go
+++ service/delete_blog_service.go
@@ -16,6 +16,11 @@ func (service *DeleteBlogService) Delete(id string) serializer.Response {
 		return serializer.ParamErr("博客不存在", err)
 	}
 	//gorm软删除
+	var userBlog model.UserBlog
+	model.DB.Where("blog_id = ?", blog.ID).First(&userBlog)
+	if err := model.DB.Delete(&userBlog).Error; err != nil {
+		return serializer.ParamErr("博客删除失败", err)
+	}
 	if err := model.DB.Delete(&blog).Error; err != nil {
 		return serializer.ParamErr("博客删除失败", err)
 	}
